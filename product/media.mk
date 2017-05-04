30dad4bd0a5c9e9014240fd5d5782e44e5822a53
Pawin Vongmasa <pawin@google.com>
2017-05-03 19:54:12 -0700

    Create media_profiles_V1_0.xml
    media_profiles_V1_0.xml is created from media_profiles.xml by replacing
    the dtd schema inside media_profiles.xml with an external reference to
    /system/etc/media_profiles_V1_0.dtd.
    
    Test: Compiles
    
    Bug: 36430270
    
    Change-Id: I2bdd38bc8e85cdb64c4729a6ebc843f5031c127c

--- product/media.mk
+++ product/media.mk
@@ -1,6 +1,6 @@
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:system/vendor/etc/media_profiles_V1_0.xml
 
 # Widevine DRM symbol, boringssl-compat subset
 PRODUCT_PACKAGES += \
