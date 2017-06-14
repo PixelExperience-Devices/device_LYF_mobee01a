f0627709a06e83427ec1a6e23ae5d058bbecd16f
Vinay Verma <vvinay@codeaurora.org>
2017-06-14 14:35:18 +0530

    Add vndk sp libraries to system lib.
    Change-Id: Ia5b0087506571a929169736dfe108e7ff3f6d40f

--- device.mk
+++ device.mk
@@ -392,5 +392,25 @@ PRODUCT_PACKAGES += \
     MiXplorer \
     MusicPlayer
 
+# VNDK
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0.vndk-sp\
+    android.hardware.graphics.allocator@2.0.vndk-sp\
+    android.hardware.graphics.mapper@2.0.vndk-sp\
+    android.hardware.graphics.common@1.0.vndk-sp\
+    android.hidl.base@1.0.vndk-sp\
+    libhwbinder.vndk-sp\
+    libbase.vndk-sp\
+    libcutils.vndk-sp\
+    libhardware.vndk-sp\
+    libhidlbase.vndk-sp\
+    libhidltransport.vndk-sp\
+    libutils.vndk-sp\
+    libc++.vndk-sp\
+    libsync.vndk-sp\
+    libbacktrace.vndk-sp\
+    libunwind.vndk-sp\
+    liblzma.vndk-sp\
+
 # Call the proprietary setup
 $(call inherit-product-if-exists, vendor/LYF/mobee01a/mobee01a-vendor.mk)
