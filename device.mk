219065c56f04fdadd68f013d8082ce465fc6a838
padarshr <padarshr@codeaurora.org>
2017-07-12 16:46:43 +0530

    Update vndk-sp build mechanism and library set.
    This change is to add some more libraries needed to
    be present in the vndk-sp library set. Also, move towards
    a better the mechanism to build vndk-sp.
    
    Change-Id: Iac86c8ece08c2dae94fce6e94deb670aa742e3dc

--- device.mk
+++ device.mk
@@ -392,25 +392,9 @@ PRODUCT_PACKAGES += \
     MiXplorer \
     MusicPlayer
 
-# VNDK
+# VNDK-SP:
 PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0.vndk-sp\
-    android.hardware.graphics.allocator@2.0.vndk-sp\
-    android.hardware.graphics.mapper@2.0.vndk-sp\
-    android.hardware.graphics.common@1.0.vndk-sp\
-    android.hidl.base@1.0.vndk-sp\
-    libhwbinder.vndk-sp\
-    libbase.vndk-sp\
-    libcutils.vndk-sp\
-    libhardware.vndk-sp\
-    libhidlbase.vndk-sp\
-    libhidltransport.vndk-sp\
-    libutils.vndk-sp\
-    libc++.vndk-sp\
-    libsync.vndk-sp\
-    libbacktrace.vndk-sp\
-    libunwind.vndk-sp\
-    liblzma.vndk-sp\
+    vndk-sp
 
 # Call the proprietary setup
 $(call inherit-product-if-exists, vendor/LYF/mobee01a/mobee01a-vendor.mk)
