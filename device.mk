9477374532091da2cf829ab7d9cffaf97a119e7d
mikeNG <mkbestas@lineageos.org>
2018-06-07 12:56:37 +0530

    LS-5015: Switch to aw2013 lights HIDL HAL implementation
    Signed-off-by: AndroiableDroid <androiabledroid@gmail.com>

--- device.mk
+++ device.mk
@@ -321,7 +321,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.lyf_8916
+    android.hardware.light@2.0-service.aw2013
 
 # Media
 PRODUCT_COPY_FILES += \
