23320c57703c7b94fb8528dcd2ea05c5d15bc493
dev-harsh1998 <harshitjain6751@gmail.com>
2018-09-11 23:56:30 +0530

    LS-5015: init: set baseband from init
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -3,15 +3,6 @@ LOCAL_PATH := $(call my-dir)
 # Configuration scripts
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := set_baseband.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/set_baseband.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.bt.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
